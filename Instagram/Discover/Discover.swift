352cd33ce1acf7f4e5624a385d5fa13ed60a0a7f
DaweiXu <listenerxdw@gmail.com>
2018-09-30 22:21:59 +1000

    creating groups

--- Instagram/Discover/Discover.swift
+++ Instagram/Discover/Discover.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class Discover: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Instagram/UserFeed/UserFeed.swift
+++ Instagram/UserFeed/UserFeed.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class UserFeed: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
